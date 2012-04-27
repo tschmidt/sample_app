d89f5c9d69077ce6f17202935e18b35e25ad928e
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-04-27 14:21:29 -0500

    Add 'Contact Us' page

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'json'
 gem 'jquery-rails', '2.0.0'
+gem 'pg', '0.12.2'
 
 group :development do
   gem 'sqlite3', '1.3.5'
@@ -12,10 +13,6 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
-end
-
-group :production do
-  gem 'pg', '0.12.2'
 end
 
 # Gems used only for assets and not required
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -9,7 +12,7 @@ describe "StaticPages" do
     
     it "should have the 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Home')
+      page.should have_selector('title', text: "#{base_title} | Home")
     end
   end
   
@@ -21,7 +24,7 @@ describe "StaticPages" do
     
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | Help')
+      page.should have_selector('title', text: "#{base_title} | Help")
     end
   end
   
@@ -33,7 +36,19 @@ describe "StaticPages" do
     
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title', text: 'Ruby on Rails Tutorial Sample App | About Us')
+      page.should have_selector('title', text: "#{base_title} | About Us")
+    end
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_content('Contact Us')
+    end
+    
+    it "should have the title 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', text: "#{base_title} | Contact Us")
     end
   end
 end
