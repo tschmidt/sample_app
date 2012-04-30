fc292c0e7fee6ae7cd5ac2e6ae23c35e536c2611
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-04-30 12:34:29 -0500

    Complete exercises for chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,25 @@ describe "StaticPages" do
     it { should have_selector('h1', text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
   end
+  
+  it "should have the right links on the layout" do
+    visit root_path
+    
+    click_link "About"
+      page.should have_selector('title', text: full_title('About Us'))
+    
+    click_link "Help"
+      page.should have_selector('title', text: full_title('Help'))
+    
+    click_link "Contact"
+      page.should have_selector('title', text: full_title("Contact Us"))
+    
+    click_link "Home"
+      page.should have_selector('title', text: full_title(''))
+    
+    click_link "Sign up now!"
+      page.should have_selector('title', text: full_title("Sign up"))
+  end
     
   describe "Home page" do
     before  { visit root_path }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,1 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  page_title ? "#{base_title} | #{page_title}" : base_title
-end
+include ApplicationHelper
