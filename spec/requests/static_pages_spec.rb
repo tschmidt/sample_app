02941ee543e58c93e71f296bb491af805d600d6a
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-04-30 12:16:13 -0500

    Update static page tests to use RSpec shared examples

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,33 +3,39 @@ require 'spec_helper'
 describe "StaticPages" do
   
   subject { page }
+  
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
     
   describe "Home page" do
     before  { visit root_path }
+    let(:heading)     { 'Sample App' }
+    let(:page_title)  { '' }
     
-    it { should have_selector('h1', text: "Sample App") }
-    it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_selector('title', text: full_title('Home')) }
   end
   
   describe "Help page" do
     before { visit help_path }
-    
-    it { should have_selector('h1', text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { heading }
+    it_should_behave_like "all static pages"
   end
   
   describe "About page" do
     before { visit about_path }
-    
-    it { should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    let(:heading)     { 'About Us' }
+    let(:page_title)  { heading }
+    it_should_behave_like "all static pages"
   end
   
   describe "Contact page" do
     before { visit contact_path }
-    
-    it { should have_selector('h1', text: 'Contact Us') }
-    it { should have_selector('title', text: full_title('Contact Us')) }
+    let(:heading)     { 'Contact Us' }
+    let(:page_title)  { heading }
+    it_should_behave_like "all static pages"
   end
 end
