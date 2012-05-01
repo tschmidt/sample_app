f68020937a93e11d38c607d901e942642469b243
Terry Schmidt <terry.m.schmidt@gmail.com>
2012-05-01 13:42:56 -0500

    Complete exercises for chapter 7

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+  
+  def default_url_options(options={})
+    { locale: params[:locale] || I18n.default_locale }
+  end
+  
+private
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+    
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,11 @@
 module UsersHelper
   
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, *args)
+    opts = args.extract_options!
+    opts[:size] ||= 90
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png"
+    gravatar_url = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{opts[:size]}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
   
